2177e1bcd03232c06966853cceb2f4001d203c77
Hiram610 <hiram.garcia216629@potros.itson.edu.mx>
2025-05-07 08:50:47 -0600

    Cambios para el numero de surcusal
    Se agrego los los textos, editor y preferencias para el numero de surcusal.

--- app/src/main/java/com/example/launcher_adf_2/Prefers.kt
+++ app/src/main/java/com/example/launcher_adf_2/Prefers.kt
@@ -8,9 +8,14 @@ import com.google.gson.reflect.TypeToken
 class Prefers (val context: Context) {
 
     val DATA_APPS = "AppsDB"
+    val DATA_NUMBER = "NumberDB"
 
     val storage = context.getSharedPreferences(DATA_APPS, 0)
+    val storageNumber = context.getSharedPreferences(DATA_NUMBER, 0)
+
     val SHARE_APP_ADMIN = "packageApps"
+    val SHARE_NUMBER_ADMIN = "numbers"
+
     var apps_User_List = ArrayList<String>()
     val gson = Gson()
 
@@ -28,6 +33,14 @@ class Prefers (val context: Context) {
     fun removeApp(app_Package: String) {
         apps_User_List.remove(app_Package)
         storage.edit() { putString(SHARE_APP_ADMIN, gson.toJson(apps_User_List)) }
+    }
+
+    fun saveNumber(number: String) {
+        storageNumber.edit() { putString(SHARE_NUMBER_ADMIN, number) }
+    }
+
+    fun getNumber() : String {
+        return storageNumber.getString(SHARE_NUMBER_ADMIN, null) ?: ""
     }
 
     fun wipe() {
